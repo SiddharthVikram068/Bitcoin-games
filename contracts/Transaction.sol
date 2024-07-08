3f5e0c9577fd6aa5267a96e3e6ac44287920e17b
Tanish-pat <Tanish.Pathania@iiitb.ac.in>
2024-07-08 23:12:19 +0530

    Changed Transaction.sol

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -5,6 +5,7 @@ contract Transaction
 {
     struct Owner 
     {
+        string ownerName;
         address ownerAddress;
         bytes32[] productHashes;
     }
@@ -23,31 +24,61 @@ contract Transaction
     mapping(address => Owner) public OwnersList;
     mapping(bytes32 => Product) public ProductsList;
 
-    event OwnerRegistered(address owner);
-    event ProductRegistered(bytes32 productHash, address owner);
-    event ProductSold(address buyer, bytes32 productHash, uint price);
+    event OwnerRegistered(address ownerAddress, string ownerName);
+    event ProductRegistered(bytes32 productHash, address ownerAddress);
+    event ProductSold(address seller, address buyer, bytes32 productHash, uint price);
     event ProductForSaleSet(bytes32 productHash, bool forSale);
+    event OwnerVerified(address ownerAddress, bytes32 productHash, bool verified);
 
-    function registerOwner(address ownerAddress) public returns (bool) 
+    // Change description of a product 
+    function changeDescription(bytes32 productHash, string memory newDescription) public returns (bool)  
+    {
+        require(ProductsList[productHash].owner == msg.sender, "Only the owner can change the description");
+        require(ProductsList[productHash].productHash != bytes32(0), "Product does not exist");
+
+        ProductsList[productHash].description = newDescription;
+
+        emit ProductRegistered(productHash, msg.sender);
+        return true;
+    }
+
+    // Change price of a product
+    function changePrice(bytes32 productHash, uint newPrice) public returns (bool) 
+    {
+        require(ProductsList[productHash].owner == msg.sender, "Only the owner can change the price");
+        require(ProductsList[productHash].productHash != bytes32(0), "Product does not exist");
+
+        ProductsList[productHash].price = newPrice;
+
+        emit ProductRegistered(productHash, msg.sender);
+        return true;
+    }
+
+    // Register a new owner
+    function registerOwner(address ownerAddress,string memory ownerName) public returns (bool) 
     {
         require(ownerAddress != address(0), "Invalid address");
         require(OwnersList[ownerAddress].ownerAddress == address(0), "Owner already registered");
 
         ownerCount++;
-        OwnersList[ownerAddress] = Owner({
+        OwnersList[ownerAddress] = Owner
+        ({
+            ownerName: ownerName,
             ownerAddress: ownerAddress,
             productHashes: new bytes32[](0)
         });
 
-        emit OwnerRegistered(ownerAddress);
+        emit OwnerRegistered(ownerAddress, ownerName);
         return true;
     }
 
+    // Generate a hash for a product
     function generateHash(address ownerAddress, uint256 number) public pure returns (bytes32) 
     {
         return keccak256(abi.encodePacked(ownerAddress, number*number));
     }
 
+    // Register a new product
     function registerProduct(address ownerAddress, string memory desc, uint256 price) public returns (bool) 
     {
         require(ownerAddress != address(0), "Invalid owner address");
@@ -71,6 +102,7 @@ contract Transaction
         return true;
     }
 
+    // Set a product for sale
     function setProductForSale(bytes32 productHash) public returns (bool) 
     {
         require(ProductsList[productHash].owner == msg.sender, "Only the owner can set the product for sale");
@@ -82,14 +114,25 @@ contract Transaction
         return true;
     }
 
+    // Verify if the owner is the owner of the product
+    function verifyOwner(address ownerAddress, bytes32 productHash) internal view returns (bool) 
+    {
+        return ProductsList[productHash].owner == ownerAddress;
+    }
+
+    // Buy a product
     function buyProduct(address seller, address buyer, bytes32 productHash) public payable returns (bool) 
     {
+        require(verifyOwner(seller, productHash), "Seller is not the owner");
+        emit OwnerVerified(seller, productHash, verifyOwner(seller, productHash));
+
         require(OwnersList[seller].ownerAddress != address(0), "Seller not registered");
         require(OwnersList[buyer].ownerAddress != address(0), "Buyer not registered");
         require(ProductsList[productHash].isForSale, "Product is not for sale");
+       
+        // Money sent should be equal to the price of the product
         require(msg.value == ProductsList[productHash].price, "Incorrect price");
         
-
         // Transfer ownership
         ProductsList[productHash].owner = buyer;
         ProductsList[productHash].isForSale = false;
@@ -103,10 +146,11 @@ contract Transaction
         // Transfer funds to seller
         payable(seller).transfer(msg.value);
 
-        emit ProductSold(buyer, productHash, msg.value);
+        emit ProductSold(seller, buyer, productHash, msg.value);
         return true;
     }
 
+    // Remove product from owner
     function removeProductFromOwner(address owner, bytes32 productHash) internal 
     {
         bytes32[] storage productHashes = OwnersList[owner].productHashes;
@@ -121,6 +165,7 @@ contract Transaction
         }
     }
 
+    // Get all products of an owner
     function getAllProductsByOwner(address ownerAddress) public view returns (Product[] memory) 
     {
         require(OwnersList[ownerAddress].ownerAddress != address(0), "Owner not registered");
